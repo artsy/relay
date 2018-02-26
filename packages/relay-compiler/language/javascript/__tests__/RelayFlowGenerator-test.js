794e23fa43d86a80b61bf8c5cdcd019aecd8d7e0
Eloy Durán <eloy.de.enige@gmail.com>
2018-02-27 00:02:59 +0100

    [language-plugin] Add test for relative import with single artifact dir.

--- packages/relay-compiler/language/javascript/__tests__/RelayFlowGenerator-test.js
+++ packages/relay-compiler/language/javascript/__tests__/RelayFlowGenerator-test.js
@@ -20,26 +20,52 @@ const parseGraphQLText = require('parseGraphQLText');
 const {transformASTSchema} = require('ASTConvert');
 const {generateTestsFromFixtures} = require('RelayModernTestUtils');
 
+import type {TypeGeneratorOptions} from '../../RelayLanguagePluginInterface';
+
+function generate(text, options: TypeGeneratorOptions) {
+  const schema = transformASTSchema(RelayTestSchema, [
+    RelayRelayDirectiveTransform.SCHEMA_EXTENSION,
+  ]);
+  const {definitions} = parseGraphQLText(schema, text);
+  return new GraphQLCompilerContext(RelayTestSchema, schema)
+    .addAll(definitions)
+    .applyTransforms(RelayFlowGenerator.transforms)
+    .documents()
+    .map(doc => RelayFlowGenerator.generate(doc, options))
+    .join('\n\n');
+}
+
 describe('RelayFlowGenerator', () => {
-  generateTestsFromFixtures(`${__dirname}/fixtures/flow-generator`, text => {
-    const schema = transformASTSchema(RelayTestSchema, [
-      RelayRelayDirectiveTransform.SCHEMA_EXTENSION,
-    ]);
-    const {definitions} = parseGraphQLText(schema, text);
-    return new GraphQLCompilerContext(RelayTestSchema, schema)
-      .addAll(definitions)
-      .applyTransforms(RelayFlowGenerator.transforms)
-      .documents()
-      .map(doc =>
-        RelayFlowGenerator.generate(doc, {
-          customScalars: {},
-          enumsHasteModule: null,
-          existingFragmentNames: new Set(['PhotoFragment']),
-          inputFieldWhiteList: [],
-          relayRuntimeModule: 'relay-runtime',
-          useHaste: true,
-        }),
-      )
-      .join('\n\n');
+  generateTestsFromFixtures(`${__dirname}/fixtures/flow-generator`, text =>
+    generate(text, {
+      customScalars: {},
+      enumsHasteModule: null,
+      existingFragmentNames: new Set(['PhotoFragment']),
+      inputFieldWhiteList: [],
+      relayRuntimeModule: 'relay-runtime',
+      useHaste: true,
+      useSingleArtifactDirectory: false,
+    }),
+  );
+
+  it('imports fragment refs from siblings in a single artifact dir', () => {
+    const text = `
+      fragment Picture on Image {
+        ...PhotoFragment
+      }
+    `;
+    const types = generate(text, {
+      customScalars: {},
+      enumsHasteModule: null,
+      existingFragmentNames: new Set(['PhotoFragment']),
+      inputFieldWhiteList: [],
+      relayRuntimeModule: 'relay-runtime',
+      // This is what's different from the tests above.
+      useHaste: false,
+      useSingleArtifactDirectory: true,
+    });
+    expect(types).toContain(
+      "import type { PhotoFragment$ref } from './PhotoFragment.graphql';",
+    );
   });
 });
